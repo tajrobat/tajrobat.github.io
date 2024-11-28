ef4a9432be9c033ada2f758364a8f4aaa7041d47
hyzenberg <hyzenberg>
2024-11-28 22:12:24 +0330

    fix type and update data from wiki

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,3 +1,5 @@
+import { Company } from "./types";
+
 export const defaultMetadata = {
   title: " تجربت | تجربیات کاری در شرکت‌های ایرانی",
   description:
@@ -6,7 +8,7 @@ export const defaultMetadata = {
     "تجربه کاری، مصاحبه شغلی، شرکت‌های ایرانی، حقوق و دستمزد، فرصت‌های شغلی",
 };
 
-export const generateCompanyMetadata = (company: any) => ({
+export const generateCompanyMetadata = (company: Company) => ({
   title: `${company.name} | نظرات و تجربیات کارمندان | تجربت`,
   description: `تجربیات کاری، مصاحبه‌های شغلی و نظرات کارمندان درباره ${company.name}. میانگین حقوق، امتیاز ${company.rate} از ۵، و ${company.total_review} نظر ثبت شده.`,
   keywords: `${company.name}, تجربه کار در ${company.name}, مصاحبه ${company.name}, حقوق ${company.name}`,
